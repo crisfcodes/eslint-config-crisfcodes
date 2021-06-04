7266d6f5c77a6325cc9b48ff470033a2fe2605a0
crisfcodes <cristianflorez0501@gmail.com>
2021-06-04 09:08:48 -0500

    refactor(example): update example with new rules

--- samples/users.tsx
+++ samples/users.tsx
@@ -28,5 +28,6 @@ export function variableNames() {
   const PascalCase = 'PascalCase'
   const __thing = '__filename'
   const CONST_VALUE = 'CONST_VALUE'
-  return {$el, user$, camelCase, PascalCase, __thing, CONST_VALUE}
+  const __DEV__ = 'global constant'
+  return {$el, user$, camelCase, PascalCase, __thing, __DEV__, CONST_VALUE}
 }
