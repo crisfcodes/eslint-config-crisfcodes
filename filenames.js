823b40c7683bc966a94be8d53b9f41f26a340d4e
Cristian Flórez <cristianflorez0501@gmail.com>
2022-02-27 22:32:35 -0500

    fix: support more extensions

--- filenames.js
+++ filenames.js
@@ -4,13 +4,13 @@ module.exports = {
   rules: {
     'filenames/match-regex': [
       'error',
-      '^\\.?[a-z-]+\\.?(config|test|queries|types)?\\.?(d)?$',
+      '^\\.?[a-z-]+\\.?(config|test|queries|types|stories|logic|layout|models|generated|styles)?\\.?(d)?$',
       true,
     ],
     'filenames/match-exported': [
       'error',
       'kebab',
-      '\\.(config|test|queries|types)$',
+      '\\.(config|test|queries|types|stories|logic|layout|models|generated|styles)$',
       true,
     ],
     'filenames/no-index': 'off',
